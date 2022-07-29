db2b2a9bad3b32ce3741ad2ed9e1d43c97ffcc19
sajon <sajonshrestha5@gmail.com>
2022-07-29 15:39:48 +0545

    2nd commit

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/details/components/title_with_more_btn.dart';
 
-import 'icon_cards.dart';
+import '../../../constants.dart';
 import 'images_and_icons.dart';
 
 class Body extends StatelessWidget {
@@ -15,6 +14,40 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           ImageAndIcons(size: size),
+          TitleAndPrice(title: "Crassula", country: "India", price: 400),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 85,
+                child: TextButton(
+                  // style: TextButton.styleFrom(
+                  //   backgroundColor: kPrimaryColor,
+                  //   primary: Colors.white,
+                  // ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    primary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.only(topRight: Radius.circular(20)),
+                    ),
+                  ),
+
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: TextButton(
+                onPressed: () {},
+                child: Text("Description"),
+              ))
+            ],
+          )
         ],
       ),
     );
